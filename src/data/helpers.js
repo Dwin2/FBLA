2c66c086a733f390f2dd43f3138fc8960875feed
Dwin2 <darwin.li@ucc.on.ca>
2023-02-26 20:11:27 -0500

    link events to fb

--- src/data/helpers.js
+++ src/data/helpers.js
@@ -2,13 +2,13 @@ import {set, ref, get, child, onValue, app, auth, database, userId, remove} from
 
 function addUserData(path, id, msgStart, msgEnd) {
     const v = ref(database, path);
-        onValue(v, (snapshot) => {
+    onValue(v, (snapshot) => {
         const data = snapshot.val();
         document.getElementById(id).innerHTML =  msgStart + data + msgEnd;
     });
 }
-function addCardBody(id) {
-    const cardBody = '<div class="card" style="margin-left: 6%;">' +
+function addCardBody(id, flag) {
+    var cardBody = '<div class="card" style="margin-left: 6%;">' +
     '<div class="card-body bg-white">' + 
     '<h5 class="card-title" id="' + id + '-name"></h5>' + 
     '<hr class="my-4">' + 
@@ -16,10 +16,17 @@ function addCardBody(id) {
     '<p class="txt" id="' + id + '-location"><strong>Location: </strong></p>' + 
     '<p class="txt" id="' + id + '-date"><strong>Date: </strong></p>' + 
     '<p class="txt" id="' + id + '-time"><strong>Time: </strong></p>' + 
-    '<a href="./login.html" class="btn btn-primary" style="margin-top: 2%; font-size: 20px; margin-right: 6%;">More Info</a>' + 
-    '<button data-toggle="modal" data-target="#modal" class="btn btn-danger" style="margin-top: 2%; font-size: 20px;" onclick="unRegister(' + "'" + id +"'" + ');">Unregister</button>' +
-    '</div>' + 
-    '</div>';
+    '<a href="./login.html" class="btn btn-primary" style="margin-top: 2%; font-size: 20px; margin-right: 6%;">More Info</a>';
+    if (!flag) {
+        cardBody += '<button data-toggle="modal" data-target="#modal" class="btn btn-danger" style="margin-top: 2%; font-size: 20px;" onclick="unRegister(' + "'" + id +"'" + ');">Unregister</button>' +
+        '</div>' + 
+        '</div>';
+    } else {
+        cardBody += '<button data-toggle="modal" data-target="#modal" class="btn btn-danger" style="margin-top: 2%; font-size: 20px;" onclick="register('+ id + ');">Register</button>' +
+        '</div>' + 
+        '</div>';
+    }
+    
     document.getElementById("cards").innerHTML += cardBody;
 }
 function addEventInfo(childData, idx) {
@@ -36,8 +43,6 @@ function addEventInfo(childData, idx) {
     });
 }
 function unRegister(id, uid) {
-    
-    console.log(id);
     const v = ref(database, 'users/' + uid + '/events/' + id);
     remove(v).then(() => {
         console.log("Unregistered.");
@@ -48,6 +53,14 @@ function unRegister(id, uid) {
     //     document.getElementById(id).innerHTML =  msgStart + data + msgEnd;
     // });
 }
+function addEvent(uid, id) {
+    console.log(id);
+    //check already registed
+    const v = ref(database, 'users/' + uid + '/events/' + id);
+    set(v, {
+        id : parseInt(id)
+    });
+}
 
-export {addUserData, addCardBody, addEventInfo, unRegister};
+export {addUserData, addCardBody, addEventInfo, unRegister, addEvent};
 
