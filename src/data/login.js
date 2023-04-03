38a42e90426b3ca474ed2c129d19b1f9f918e71e
N Cheng <nicholas.cheng@ucc.on.ca>
2023-04-03 09:46:44 -0400

    Update login.js

--- src/data/login.js
+++ src/data/login.js
@@ -52,7 +52,7 @@ function signUp() {
               // Data saved successfully!
               alert('User created successfully');
               //alert(user.password); 
-              window.location.href = "/FBLA/index.html";
+              window.location.href = "./pages/home.html";
             })
             .catch((error) => {
               // The write failed...
